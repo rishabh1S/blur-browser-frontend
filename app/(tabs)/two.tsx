e50a6e935de7315dc964734a55376fdb695723f5
rishabh1S <rishabh15112001@gmail.com>
2024-12-21 22:04:08 +0530

    feat: setup rn reusables

---
+++ app/(tabs)/two.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function TabTwoScreen() {
+  return (
+    <View className="flex flex-1 items-center justify-center">
+      <Text className="text-xl font-bold dark:text-white">Tab Two</Text>
+    </View>
+  );
+}
